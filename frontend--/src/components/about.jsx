9b5de710c0dece4268e06ec782495cbf9ad4c44a
bearsyker <bearsyker@gmail.com>
2024-04-22 05:06:13 -0300

    about and products front

--- frontend--/src/components/about.jsx
+++ frontend--/src/components/about.jsx
@@ -1,28 +1,21 @@
 function About() { 
     return (
-        <section id="about">
-            <p class="section__text__p1">Get To Know More</p>
-            <h1 class="title">About</h1>
-            <div class="section-container">
-            
-            <div class="about-details-container">
-                <div class="about-containers">
-                <div class="details-container">
-                    
-                </div>
-                
-                </div>
-                <div class="text-container">
-                <p>
+        <section className="sec-about">
+            <div className="about">
+                <p className="p1">Get To Know More</p>
+                <h2 className="title">About</h2>
+                <h4>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis
                     reprehenderit et laborum, rem, dolore eum quod voluptate
                     exercitationem nobis, nihil esse debitis maxime facere minus sint
                     delectus velit in eos quo officiis explicabo deleniti dignissimos.
                     Eligendi illum libero dolorum cum laboriosam corrupti quidem,
                     reiciendis ea magnam? Nulla, impedit fuga!
-                </p>
-                </div>
+                </h4>
             </div>
+
+            <div className="show">
+                <h2>hello</h2>
             </div>
         </section>
     )
--- frontend--/src/components/products.jsx
+++ frontend--/src/components/products.jsx
@@ -1,6 +1,85 @@
 function Products() { 
     return (
-        <div>hello</div>
+        <section id="product1" className="sec-1">
+            <h2>Featured Products</h2>
+            <p>modern home design</p>
+
+            <div className="pr1-cont">
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+                <div className="cont">
+                    <img src="assets/furn-img.webp" alt="" />
+                    <div className="desc">
+                        <span>furniture</span>
+                        <h5>black carpet or smth</h5>
+                        <h4>$1000</h4>
+                    </div>
+                    <button className="check">check</button>
+                </div>
+            </div>
+
+        </section>
 
 
     ) 
